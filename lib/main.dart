69d310c99af317f983f345f7a07c635f6ad03e6c
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-03-26 16:39:20 +0530

    today Flutter practice

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/screens/main_menu_screen.dart';
+import 'package:tictactoe/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: bgcolor,
+      ),
+      home: const MainMenuScreen(),
+    );
+  }
+}
---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:Text('main menu'),
+      ),
+    );
+  }
+}
